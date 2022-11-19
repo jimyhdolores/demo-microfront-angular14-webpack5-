8e1f81d9da971e2fef8688e62398f9d4dcb8df0e
jimmy <jimmyhd_19@hotmail.com>
2022-11-19 09:59:27 -0500

    init

--- projects/mf-shopping/src/app/app.module.ts
+++ projects/mf-shopping/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  // imports: [BrowserModule, AppRoutingModule],
-  imports: [BrowserModule],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
